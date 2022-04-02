06f8ad5a585f1b93e4147f7eaa47ac9805a1f52a
Ena Habensus <eh3066@g.rit.edu>
2022-04-03 00:46:24 +0200

    commit

---
+++ src/build/precache-manifest.43d8d15fa5d89bee3137353e21fcdf3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df51779e10b8a192b1cd",
+    "url": "/static/css/main.259cbfda.chunk.css"
+  },
+  {
+    "revision": "df51779e10b8a192b1cd",
+    "url": "/static/js/main.057887b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c41ad2443ecee5c769f3",
+    "url": "/static/js/2.ba5cc21a.chunk.js"
+  },
+  {
+    "revision": "c6059cd6ac45a295ba81bb5771706d8c",
+    "url": "/index.html"
+  }
+];
